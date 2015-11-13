4dc4b5289e00ee30c8508df919b8c7155ee517d2
Brendan <bjq6@cornell.edu>
2015-11-12 21:53:25 -0500

    more

--- database.mli
+++ database.mli
@@ -1,18 +1,47 @@
+open Controller
+
 (*Define all the units here*)
 
-type unit_type =  Infrantry  | Ocamlry  | Tank 
-type unit_parameters = {
-  max_hp : int;
-  max_mvt : int;
-  unit_cost : int;
-  typ : game_unit; 
-  unit_id : int;
-  position : mutable int*int;
-  curr_hp : mutable int;
-  movement_capabailit : mutable int;
-  active : mutable bool
+type unit_type =  Infrantry  | Ocamlry  | Tank
+
+type base_unit = {
+  unit_typ : unit_type; (* Type of unit*)
+  max_hp : int;         (* Max health for a given type of unit *)
+  max_mvt : int;        (* Max movement for a given type of unit*)
+  unit_cost : int;      (* The cost of purchasing a single unit of said type*)
 }
 
-val update_state : state -> state
+type unit_parameters = {
+  typ : unit_type;
+  unit_id : int;
+  active : mutable bool;
+  curr_hp : mutable int;
+  curr_mvt : mutable int;
+  position : mutable int*int
+}
+
+type building_parameters = {
+  builing_id : int;
+  max_hp : int;
+  curr_hp : mutable int;
+  active : mutable bool;
+}
+
+type tile = Plain | Water | Building
+
+type player = {
+  money : int;
+  building_ids : list of ints;
+  unit_ids : list of ints;
+}
+
+type gamestate = {
+  map : list of list of tile;
+  player_state : list of player;
+  unit_list : list of unit_parameters;
+  building_list : list of building_parameters;
+}
+
+val update_state : gamestate -> gamestate
 
 
