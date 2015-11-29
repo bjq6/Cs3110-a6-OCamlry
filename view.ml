657c87a7b4873b7ee3856356b67f2694661dc734
Oscar Zegarra <oaz2@cornell.edu>
2015-11-28 22:37:27 -0500

    More Degubbing/Trying to make it compile.  Need a working copy of database.mli to do so however....

--- view.ml
+++ view.ml
@@ -1,22 +1,10 @@
-let previous_state = ref gamestate in 
-let curr_x = ref 0 in 
-let water_color = Graphics.rgb 0 191 255 in 
-let plain_color = Graphics.rgb 189 183 107 in 
- 
-let init gamestate =  
-  create_map gamestate; 
-  populate_map gamestate
+open Graphics
+open Database 
 
-let rec observe_gamestate gamestate = 
-  upon gamestate.updated (fun updated_state ->
-    depopulate_map previous_state;
-    populate_map updated_state;
-    update_status_bar;
-    previous_state := update_state)
-    
-  
-let update_grapic gamestate = failwith "unimplemented"
-
+let previous_state = ref gamestate  
+let curr_x = ref 0  
+let water_color = Graphics.rgb 0 191 255 
+let plain_color = Graphics.rgb 189 183 107 
 
 
 let rec process_tyles map : tyle_type array array = failwith "unimplemented"
@@ -29,15 +17,15 @@ let draw_tyle tyle =
   Graphics.set_color= match e with Plain -> plain_color |Water -> water_color;
   Graphics.draw_rect 
 
-let get_array_map_to_draw tyle_array delta_x : tyle_type array = 
-  Arrays.Map tyle_array (fun e -> 
+let get_array_map_to_draw tyle_array delta_x = 
+  Array.map ( fun e -> 
     curr_x := !curr_x + delta_x;
     { x = !curr_x; y= !curr_y; height = h; witdth = w;
-    tyle_color = match e with Plain -> plain_color | Water -> water_color})
+    tyle_color = match e with Plain -> plain_color | Water -> water_color})  tyle_array
 
 let get_matrix_map_to_draw tyle_matrix delta_x delta_y 
                           : tyle_type array array = 
-  Arrays.Map tyle_matrix (fun arr_lst -> 
+  Array.map tyle_matrix (fun arr_lst -> 
     curr_x := -delta_x;
     curr_y := !curr_y + delta_y;
     get_array_map_to_draw arr_lst delta_x)
@@ -65,10 +53,21 @@ let create_map gamestate =
   let processed_map = get_matrix_map_to_draw gamestate.map delta_x delta_y in
   (*At this point, we should now have a map that is processed and ready to get
    * filed*)
-  draw_map processed_map;
+  draw_map processed_map
 
 let populate_map gamestate = failwith "unimplemented"
 
 let depopulate_map gamestate = failwith "unimplemented"
 
 let update_status_bar gamestate = failwith "unimplemented"
+
+let observe_gamestate gamestate = 
+  upon gamestate.updated (fun updated_state ->
+    depopulate_map previous_state;
+    populate_map updated_state;
+    update_status_bar;
+    previous_state := update_state)
+ 
+let init gamestate =  
+  create_map gamestate; 
+  populate_map gamestate
