d353f208bbba9a6db6cf73381d2d2a822dfb248e
matt <meh293@cornell.edu>
2015-11-30 23:56:12 -0500

    add sim_battle which return what health would be lost if these units did battle.

--- battle.mli
+++ battle.mli
@@ -3,5 +3,6 @@ open Random
 
 val battle : unit_parameters-> unit_parameters->
               unit_parameters list-> unit_parameters list
+val sim_battle : unit_parameters-> unit_parameters -> (int*int)
 val capture : player_id -> building_parameters ->
               building_parameters list -> building_parameters list
