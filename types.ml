c1ed7fb8b7fbcb7bc023d413a7d4efc54b88536f
brendan <bjq66@cornell.edu>
2015-11-30 23:39:03 -0500

    just cleaning

--- types.ml
+++ types.ml
@@ -35,7 +35,7 @@ type unit_parameters = {
   mutable active : bool;   (* Reflects whether the unit acted on the given turn *)
   mutable curr_hp : int;  (* Reflects the current health of the unit*)
   mutable curr_mvt : int; (* Reflects how many more moves the unit has this turn*)
-  mutable position : int*int;  (* A location where the unit is now *)
+  mutable position : loc;  (* A location where the unit is now *)
 }
 
 (* Stats of a given building *)
@@ -43,7 +43,7 @@ type building_parameters = {
   max_hp : int;               (* Max health of a building *)
   mutable owner : player_id;  (* Player who owns it *)
   mutable curr_hp : int;      (* Current health of this building *)
-  mutable position : int*int; (* A location where the building is on a map *)
+  mutable position : loc;     (* A location where the building is on a map *)
 }
 
 
