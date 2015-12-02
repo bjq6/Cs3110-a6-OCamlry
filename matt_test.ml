da90d7a03fa27fdd66ce2038c2b446412658798a
Alix Katague <ack242@cornell.edu>
2015-12-01 22:29:17 -0500

    hi view will now compile with cs3110 compile

--- matt_test.ml
+++ matt_test.ml
@@ -1,5 +1,11 @@
 open Getcmd
 open Types
+open View
+open Controller
+open Util
+
+
+let _ = Plain in
 
 match getcmd("1") with
 |Invalid s ->Printf.printf "Wrong: %s\n" s
@@ -9,3 +15,33 @@ match getcmd("1") with
 |EndTurn -> Printf.printf "You have Ended Your turn\n"
 |Capture (x,y)-> Printf.printf "Capture (%d,%d)\n" x y
 |Buy (x,(a,b)) -> Printf.printf "Buy %s at (%d,%d)\n" (unit2str x) a b
+
+let create_dummy_unit owner x y typ =
+  {typ = typ; plyr = owner; unit_id= 0; active = true; curr_hp = 250;
+  curr_mvt = 100; position = (x,y);}
+
+let test_init_state3 = {
+  map = [|[|Building (Some (Player2 "Yellow")); Plain; Plain; Water; Plain; Plain; Plain; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Building (Some (Player1 "Matt")); Plain; Plain; Plain; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Water; Plain; Plain; Plain; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Water; Plain; Plain; Plain; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Plain; Plain; Water; Water; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Plain; Plain; Water; Water; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Plain; Plain; Water; Water; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Plain; Plain; Water; Water; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Plain; Plain; Plain; Plain; Plain; Plain; Plain|];
+          [|Plain; Plain; Plain; Plain; Plain; Plain; Plain; Building (Some (Player2 "Yellow")); Plain; Plain|]
+          |];
+  curr_player = {player_name = Player1 "Matt"; money= 0; score = 0};
+  player_state = [];
+  unit_list =
+    [create_dummy_unit (Player1 "Matt") 0 0 (Infantry);
+     create_dummy_unit (Player1 "Matt") 1 0 (Tank);
+     create_dummy_unit (Player1 "Matt") 1 1 (Ocamlry);
+     create_dummy_unit (Player2 "Yellow") 9 9 (Infantry);
+     create_dummy_unit (Player2 "Yellow") 9 8 (Tank);
+     create_dummy_unit (Player2 "Yellow") 9 7 (Ocamlry)];
+  building_list = [];
+}
+let () = init (test_init_state3) in
+main (test_init_state3)
--- view.ml
+++ view.ml
@@ -1,47 +1,49 @@
-(*Define the colors and variables that will be used in the game.  These 
+open Graphics
+open Types
+(*Define the colors and variables that will be used in the game.  These
  * include tyle colors, active/inactive unit colors, and neutral colors
  *)
-let curr_x = ref 0 
-let curr_y = ref 0 
-let water_color = Graphics.cyan 
-let plain_color = Graphics.rgb 153 255 153 
+let curr_x = ref 0
+let curr_y = ref 0
+let water_color = Graphics.cyan
+let plain_color = Graphics.rgb 153 255 153
 let player1_color = Graphics.red
-let player1_inactive_color = Graphics.rgb 201 136 136 
+let player1_inactive_color = Graphics.rgb 201 136 136
 let player2_color = Graphics.yellow
 let player2_inactive_color = Graphics.rgb 140 140 212
 let neutral_color = Graphics.white
-(*The followind two functions below should take in the raw game map 
- * (which at this point is an array of array and then return a new 
+(*The followind two functions below should take in the raw game map
+ * (which at this point is an array of array and then return a new
  * array matrix, this matrix will be used to draw the map
  *)
-let draw_tyle tyle delta_x h w= 
-  Graphics.set_color (match tyle with Plain -> plain_color 
+let draw_tyle tyle delta_x h w=
+  Graphics.set_color (match tyle with Plain -> plain_color
     |Water -> water_color
     |Building (None) -> neutral_color
     |Building (Some (Player1 _)) -> player1_color
     |Building (Some (Player2 _)) -> player2_color);
-  Graphics.fill_rect !curr_x !curr_y h w 
+  Graphics.fill_rect !curr_x !curr_y h w
 
-let draw_array tyle_array delta_x h w = 
-  Array.iter ( fun e -> 
+let draw_array tyle_array delta_x h w =
+  Array.iter ( fun e ->
     curr_x := !curr_x + delta_x;
     draw_tyle e delta_x h w)  tyle_array
 
-let draw_map tyle_matrix delta_x delta_y  = 
-  Array.iter (fun arr_lst -> 
+let draw_map tyle_matrix delta_x delta_y  =
+  Array.iter (fun arr_lst ->
     curr_x := -delta_x;
     curr_y := !curr_y + delta_y;
     draw_array arr_lst delta_x delta_y delta_x) tyle_matrix
 
-let draw_hort_lines delta_x delta_y = 
+let draw_hort_lines delta_x delta_y =
   Graphics.set_color Graphics.black;
   Graphics.moveto 0 0;
-  for i = 0 to 600/delta_x do 
+  for i = 0 to 600/delta_x do
     Graphics.moveto 0 (Graphics.current_y () + delta_y);
     Graphics.lineto 600 (Graphics.current_y ());
   done
 
-let draw_vert_lines delta_x delta_y = 
+let draw_vert_lines delta_x delta_y =
   Graphics.set_color Graphics.black;
   Graphics.moveto 0 0;
   for i = 0 to 600/delta_y do
@@ -49,7 +51,7 @@ let draw_vert_lines delta_x delta_y =
     Graphics.lineto (Graphics.current_x ()) 600;
   done
 
-let draw_grid delta_x delta_y = 
+let draw_grid delta_x delta_y =
   draw_vert_lines delta_x delta_y;
   draw_hort_lines delta_x delta_y
 
@@ -57,15 +59,15 @@ let draw_grid delta_x delta_y =
  * 1) Create the nXn grid where that is determined by some other modules
  * 2) Using the gamestate, color the grid based on terrain
  *)
-let create_map gamestate = 
+let create_map gamestate =
   Graphics.open_graph " 600x650";
-  
+
   (*Define colors for the water and plain tiles
    *)
   (*First, define what delta_x and delta_y will be*)
   let num_vert_tiles = Array.length gamestate.map in
   let num_hor_tiles = Array.length gamestate.map.(0) in
-  let delta_x = 600/num_hor_tiles in 
+  let delta_x = 600/num_hor_tiles in
   let delta_y = 600/num_vert_tiles in
   curr_y := -delta_y;
   draw_map gamestate.map delta_x delta_y;
@@ -74,59 +76,59 @@ let create_map gamestate =
    * filed*)
 (*Helper functions to use when drawing units in the map *)
 (*Buildings will be represented as triangles*)
-let draw_building (x,y) delta_x delta_y = 
-  let height = delta_y*8/10 in 
+let draw_building (x,y) delta_x delta_y =
+  let height = delta_y*8/10 in
   let width = delta_x*2/10 in
-  Graphics.fill_rect (delta_x*x + delta_x*4/10) (delta_y*y + delta_y/10) 
-                      width height 
-                      
+  Graphics.fill_rect (delta_x*x + delta_x*4/10) (delta_y*y + delta_y/10)
+                      width height
+
 (*infantry is represented by triangle*)
-let draw_infantry (x,y) delta_x delta_y = 
+let draw_infantry (x,y) delta_x delta_y =
   Graphics.fill_poly [|(x*delta_x + delta_x/10, y*delta_y + delta_y/10);
                        ((x+1)*delta_x - delta_x/10, y*delta_y + delta_y/10);
                        (x*delta_x + delta_x/2, (y+1)*delta_y - delta_y/10) |]
 
 (*tank is represented by a circle*)
-let draw_tank (x,y) delta_x delta_y = 
-  Graphics.fill_circle (delta_x*x + delta_x/2) 
+let draw_tank (x,y) delta_x delta_y =
+  Graphics.fill_circle (delta_x*x + delta_x/2)
                         (delta_y*y + delta_y/2) (delta_x*4/10)
 
 (*ocamlry is represetned by square*)
 let draw_ocamlry (x,y) delta_x delta_y =
-  Graphics.fill_rect (delta_x*x + 6) (delta_y*y + 6) 
+  Graphics.fill_rect (delta_x*x + 6) (delta_y*y + 6)
                       (delta_x*8/10) (delta_y*8/10)
 
 (*infantry is represented by triangle*)
-let draw_infantry_outline (x,y) delta_x delta_y = 
+let draw_infantry_outline (x,y) delta_x delta_y =
   Graphics.draw_poly [|(x*delta_x + delta_x/10, y*delta_y + delta_y/10);
                        ((x+1)*delta_x - delta_x/10, y*delta_y + delta_y/10);
                        (x*delta_x + delta_x/2, (y+1)*delta_y - delta_y/10) |]
 
 (*tank is represented by a circle*)
-let draw_tank_outline (x,y) delta_x delta_y = 
-  Graphics.draw_circle (delta_x*x + delta_x/2) 
+let draw_tank_outline (x,y) delta_x delta_y =
+  Graphics.draw_circle (delta_x*x + delta_x/2)
                         (delta_y*y + delta_y/2) (delta_x*4/10)
 
 (*ocamlry is represetned by square*)
 let draw_ocamlry_outline (x,y) delta_x delta_y =
-  Graphics.draw_rect (delta_x*x + 6) (delta_y*y + 6) 
+  Graphics.draw_rect (delta_x*x + 6) (delta_y*y + 6)
                       (delta_x*8/10) (delta_y*8/10)
 (*Function that will return a color based on (1) the player who owns
  * the color and (2) whether the unit is active or not
  *)
-let get_color unit_to_draw = 
-  match (unit_to_draw.plyr, unit_to_draw.active) with 
+let get_color unit_to_draw =
+  match (unit_to_draw.plyr, unit_to_draw.active) with
   |(Player1 _, true) -> player1_color
-  |(Player1 _, false) -> player1_inactive_color 
+  |(Player1 _, false) -> player1_inactive_color
   |(Player2 _, true) -> player2_color
   |(Player2 _, false) -> player2_inactive_color
 
 
-let draw_unit_status (x,y) hp delta_x delta_y = 
+let draw_unit_status (x,y) hp delta_x delta_y =
   Graphics.moveto (x*delta_x + delta_x*3/10) (y*delta_y + delta_y*3/10);
   Graphics.draw_string (string_of_int hp)
 
-let draw_unit unit_to_draw delta_x delta_y = 
+let draw_unit unit_to_draw delta_x delta_y =
   Graphics.set_color (get_color unit_to_draw);
   (match unit_to_draw.typ with
   |Infantry -> draw_infantry unit_to_draw.position delta_x delta_y
@@ -138,28 +140,28 @@ let draw_unit unit_to_draw delta_x delta_y =
   |Ocamlry  -> draw_ocamlry_outline unit_to_draw.position delta_x delta_y
   |Tank     -> draw_tank_outline unit_to_draw.position delta_x delta_y );
   draw_unit_status unit_to_draw.position (unit_to_draw.curr_hp) delta_x delta_y
-   
 
-let populate_map gamestate = 
-  let delta_x = 600/(Array.length gamestate.map) in 
+
+let populate_map gamestate =
+  let delta_x = 600/(Array.length gamestate.map) in
   let delta_y = 600/(Array.length gamestate.map.(0)) in
-  List.iter (fun elt -> draw_unit elt delta_x delta_y) gamestate.unit_list 
+  List.iter (fun elt -> draw_unit elt delta_x delta_y) gamestate.unit_list
 
 let depopulate_map gamestate = create_map gamestate
 
 let update_status_bar gamestate = failwith "unimplemented"
 
-(*let observe_gamestate gamestate = 
+(*let observe_gamestate gamestate =
   upon gamestate.update (fun updated_state ->
     depopulate_map previous_state;
     populate_map updated_state;
     update_status_bar;
     previous_state := update_state)
 *)
-let update_state new_gamestate = 
+let update_state new_gamestate =
   depopulate_map new_gamestate;
   populate_map new_gamestate
 
-let init gamestate =  
-  create_map gamestate; 
+let init gamestate =
+  create_map gamestate;
   populate_map gamestate
