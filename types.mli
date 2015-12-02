f7acd3f1c015a4731377034a12552c46c49e7c56
Alix Katague <ack242@cornell.edu>
2015-12-01 20:43:04 -0500

    oops forgot to push mli

--- types.mli
+++ types.mli
@@ -9,8 +9,6 @@ type player_id = Player1 of string | Player2 of string
 type player = {
   player_name : player_id;      (* who this player is *)
   mutable money : int;                  (* How much money this player has*)
-  mutable building_ids : int list;  (* All the buildings owned by this player *)
-  mutable unit_ids : int list;      (* All the units owned by this player*)
 }
 
 (* Types of unit - all units start with the same base stats *)
